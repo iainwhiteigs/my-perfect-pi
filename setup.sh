abe857e3f15aaa5c56d4329d5e0bb57c83b920ef
Iain White <107495931+iainwhiteigs@users.noreply.github.com>
2023-11-01 22:11:40 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-# Copilot
+# Cockpit for remote management
+sudo apt -y install cockpit
 
-# oh-my-zsh
-#  autocomplete
+#### Oh My Zsh ####
+sudo apt -y install zsh
+
+# Set zsh as default shell
+chsh -s $(which zsh)
+
+# Install Oh My Zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install fonts-powerline
+sudo apt -y install fonts-powerline
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#enable plugins
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+# Powerlevel 10K
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# use sed to replace ZSH_THEME="robbyrussell" with ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+
+#### Docker ####
+sudo apt -y install docker.io
